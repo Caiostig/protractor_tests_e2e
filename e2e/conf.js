e6d77da712d3200e886d97713dba91ec4fd6609d
Caiostig <caio.stig@gmail.com>
2019-07-29 19:33:10 -0300

    Arquivo de configuração atualizado

--- e2e/conf.js
+++ e2e/conf.js
@@ -7,6 +7,12 @@ switch (TEST_ENV) {
   case 'local':
     environmentParameters = Data[0].local
     break
+  case 'localDev':
+    environmentParameters = Data[0].localDev
+    break
+  case 'localHom':
+    environmentParameters = Data[0].localHom
+    break
 }
 
 exports.config = {
@@ -14,13 +20,22 @@ exports.config = {
   ignoreUncaughtExceptions: true,
   framework: 'custom',
   frameworkPath: require.resolve('protractor-cucumber-framework'),
-  restartBrowserBetweenTests: false,
+  restartBrowserBetweenTests: false, //mata as instâncias a cada cenário que roda
   getPageTimeout: 50000,
   allScriptsTimeout: 30000,
+  highlightDelay: 1600, //diminuir a velocidade dos testes e faz um debug préveo visualmente de onde ele vai fazer a ação
   rootElement: '*[ng-app]',
   baseUrl: environmentParameters.baseUrl,
   params: {
+    // Aqui posso criar credenciais de login
 
+    // loginCredentials: {
+    //   'userxx': 'xxx.zzz.yyy-tt',
+    //   'passwordAA': 'senha01',
+
+    //   'userxy': 'xxx.zzz.yyy-tt',
+    //   'passwordAF': 'senha02'
+    // }
   },
 
   specs: [
@@ -34,15 +49,18 @@ exports.config = {
     'browserName': 'chrome',
     chromeOptions: {
       args: [
+        '--no-sandbox',
         '--disable-gpu',
-        '--start-fullscreen'
+        '--window-size=1280x1024',
+        // '--headless'
       ]
     }
   },
 
   cucumberOpts: {
     require: '../features/step_definitions/*.js',
-    tags: ['@run'],
+    // require: '../features/support/*.js',
+    tags: ['~@notImplemented', '~@backLog', '~@notRun', '@automated'],
     format: ['json:results.json'],
     profile: false,
     'no-source': true
@@ -52,7 +70,7 @@ exports.config = {
     // Use only for angular applications
     // False: app Angular
     // True: app not Angular
-    browser.ignoreSynchronization = false
+    browser.ignoreSynchronization = true
   },
 
   afterLaunch: function () {
